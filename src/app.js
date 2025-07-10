25ee8e2f6b864faf3018f80f3235ae337aa362b8
aalia021 <aalia9209@gmail.com>
2025-07-10 10:17:37 +0530

    port config

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ connectDB()
   .then(() => {
     console.log("Database connection established ...");
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT;
     app.listen(PORT, () => {
       console.log(`Server is successfully listening on port ${PORT}`);
     });
